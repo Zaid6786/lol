82bbf00d77f0195919f3dba95328aef8e7caac6d
beast_zaid <mohammedzaidfs@gmail.com>
2026-07-27 00:03:00 +0530

    Fix environment import paths in frontend services

--- frontend/frontend/bus-tracker-app/src/app/services/complaint.service.ts
+++ frontend/frontend/bus-tracker-app/src/app/services/complaint.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, BehaviorSubject, of, throwError } from 'rxjs';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../environments/environment';
 
 export interface Complaint {
   complaintId?: number;
